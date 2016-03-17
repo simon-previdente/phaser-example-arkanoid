edfded09cc4accd2c0e9cbbd3e78d55bd2b7938b
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-03-17 11:03:31 +0100

    🚧  [tech/init-repository] : Bundle JS with livereloading

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ var del = require('del');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var plumber = require('gulp-plumber');
+var template = require('gulp-template');
+var kebabCase = require('lodash/string/kebabCase');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 
 var pkg = require('./package.json');
 
@@ -19,6 +25,11 @@ var paths = {
   assets: {
     src: pkg.project.source + pkg.project.bundle.assets,
     dest: pkg.project.build + pkg.project.bundle.assets
+  },
+  js: {
+    index: pkg.project.source + '/game/index.js',
+    bundle: 'bundle-' + pkg.name + '.js',
+    dest: pkg.project.build
   }
 }
 
@@ -34,7 +45,29 @@ gulp.task('clean', function(cb) {
  */
 gulp.task('build-index', function() {
   return gulp.src(paths.index.src)
+    .pipe(plumber())
+    .pipe(template({
+      title: pkg.name,
+      bundle: paths.js.bundle,
+      gameId: kebabCase(pkg.name)
+    }))
     .pipe(gulp.dest(paths.index.dest))
+    .pipe(reload({stream: true}));
+});
+
+/**
+ * Build js
+ */
+gulp.task('build-js', function() {
+
+  var bundleStream = browserify(paths.js.index)
+    .bundle()
+    .on('error', function logError(error) {
+      gutil.log(gutil.colors.red('BUNDLE JS'), error.toString());
+      this.emit('end');
+    })
+    .pipe(source(paths.js.bundle))
+    .pipe(gulp.dest(paths.js.dest))
     .pipe(reload({stream: true}));
 });
 
@@ -60,13 +93,14 @@ gulp.task('serve', function() {
   });
 
   gulp.watch([paths.index.src], ['build-index']);
+  gulp.watch([paths.js.index], ['build-js']);
 
 });
 
 /**
  * Build game
  */
-gulp.task('build', ['build-index', 'copy-assets']);
+gulp.task('build', ['build-index', 'build-js', 'copy-assets']);
 
 /**
  * DEFAULT
